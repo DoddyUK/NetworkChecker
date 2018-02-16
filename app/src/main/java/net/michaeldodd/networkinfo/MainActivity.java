4b0e9becbf723df3de6d2494e260e540da5db1df
Michael Dodd <michael@Rei.home>
2018-02-16 23:20:30 +0000

    Added menu option Adapter now refreshes Icons for cards

--- app/src/main/java/net/michaeldodd/networkinfo/MainActivity.java
+++ app/src/main/java/net/michaeldodd/networkinfo/MainActivity.java
@@ -2,18 +2,28 @@ package net.michaeldodd.networkinfo;
 
 import android.content.Context;
 import android.net.ConnectivityManager;
+import android.net.LinkAddress;
 import android.net.LinkProperties;
 import android.net.Network;
+import android.net.NetworkInfo;
+import android.net.RouteInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import net.michaeldodd.networkinfo.adapter.NetworkRecyclerAdapter;
+import net.michaeldodd.networkinfo.model.NetworkInterfaceInfo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static net.michaeldodd.networkinfo.model.NetworkInterfaceInfo.create;
 
 public class MainActivity extends AppCompatActivity {
-
-    private RecyclerView networkList;
     private final NetworkRecyclerAdapter adapter = new NetworkRecyclerAdapter();
 
     @Override
@@ -21,19 +31,60 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        networkList = findViewById(R.id.lst_network_interfaces);
+        RecyclerView networkList = findViewById(R.id.lst_network_interfaces);
         networkList.setLayoutManager(new LinearLayoutManager(this));
         networkList.setAdapter(adapter);
+
+        getConnectionInfo();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.menu_refresh:
+                getConnectionInfo();
+                break;
+        }
+
+        return true;
     }
 
     private void getConnectionInfo() {
         ConnectivityManager manager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
         if (manager == null) return;
 
+        List<NetworkInterfaceInfo> interfaces = new ArrayList<>();
+
         for (Network network : manager.getAllNetworks()) {
+            NetworkInfo info = manager.getNetworkInfo(network);
             LinkProperties properties = manager.getLinkProperties(network);
 
+            List<LinkAddress> links = properties.getLinkAddresses();
+            List<RouteInfo> routes = properties.getRoutes();
+
+            String addr = links.isEmpty() ? "" : links.get(0).getAddress().getHostAddress();
+            String gateway = routes.isEmpty() ? "" : routes.get(0).getGateway().getHostAddress();
+
+            interfaces.add(create(
+                    info.getType(),
+                    properties.getInterfaceName(),
+                    info.getTypeName(),
+                    addr,
+                    gateway,
+                    false
+            ));
         }
 
+        adapter.setItems(interfaces);
+        adapter.notifyDataSetChanged();
     }
+
+
 }
--- app/src/main/java/net/michaeldodd/networkinfo/model/NetworkInterfaceInfo.java
+++ app/src/main/java/net/michaeldodd/networkinfo/model/NetworkInterfaceInfo.java
@@ -3,7 +3,14 @@ package net.michaeldodd.networkinfo.model;
 import com.google.auto.value.AutoValue;
 
 @AutoValue public abstract class NetworkInterfaceInfo {
+    public abstract int type();
     public abstract String name();
+    public abstract String typeName();
     public abstract String address();
     public abstract String gateway();
+    public abstract boolean isIPV6();
+
+    public static NetworkInterfaceInfo create(int type, String name, String typeName, String address, String gateway, boolean isIPV6) {
+        return new AutoValue_NetworkInterfaceInfo(type, name, typeName, address, gateway, isIPV6);
+    }
 }
